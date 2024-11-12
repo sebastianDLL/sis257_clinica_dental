ff4ade8356a4c4d03d906e08be615c6f311c47c6
Sunset <bejarano.chara.josedavid@usfx.bo>
2024-11-12 14:40:48 -0400

    update repositorio (auth)

--- backend_clinica_dental/src/auth/dto/auth-login.dto.ts
+++ backend_clinica_dental/src/auth/dto/auth-login.dto.ts
@@ -5,7 +5,7 @@ export class AuthLoginDto {
   @ApiProperty()
   @IsNotEmpty({ message: 'El campo Email no debe ser vacío' })
   @IsString({ message: 'El campo Email debe ser de tipo cadena' })
-  @MaxLength(20, { message: 'El campo Email excede los 20 caracteres' })
+  @MaxLength(80, { message: 'El campo Email excede los 80 caracteres' })
   @MinLength(4, { message: 'El campo Email es menor a 4 caracteres' })
   @IsEmail({}, { message: 'El campo Email no es un correo válido' })
   email: string;
